dbb486b8d7dd1bb3c6380327b99011e6778b14e5
Martin Ström <my-name@my-domain.se>
2009-03-23 19:00:35 +0100

    removed old comment

--- lib/things.rb
+++ lib/things.rb
@@ -3,9 +3,6 @@ $:.unshift File.dirname(__FILE__)
 require "rubygems"
 require "hpricot"
 
-# temp#######
-# require '/Users/martin/Code/rails/head/activesupport/lib/active_support/core_ext/module/attribute_accessors'
-
 class Symbol
   def to_proc
     Proc.new { |obj, *args| obj.send(self, *args) }
